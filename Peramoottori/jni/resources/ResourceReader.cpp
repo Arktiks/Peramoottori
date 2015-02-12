e44a42a05b568e6c7ea17543ce9d23cec608d066
Grimcode <tti13stuukkak@kamk.fi>
2015-02-12 13:55:47 +0200

    PNG + Text reading tehty, pitää vielä testata.

--- Peramoottori/jni/resources/ResourceReader.cpp
+++ Peramoottori/jni/resources/ResourceReader.cpp
@@ -1,4 +1,7 @@
 #include "ResourceReader.h"
+#include <system\PMdebug.h>
+#include <system\PMassert.h>
+
 #include <lodepng.h>
 #include <vector>
 
@@ -11,19 +14,20 @@ ResourceReader* ResourceReader::GetInstance(AAssetManager* manager)
 	{
 		instance = new ResourceReader;
 		instance->manager = manager;
+		PMdebug::MsgInfo("ResourceReader initialized and manager assigned.");
 	}
-	else if (instance != nullptr && manager == nullptr)
+	else if (instance != nullptr && instance->manager == nullptr)
 	{
 		instance->manager = manager;
+		PMdebug::MsgInfo("ResourceReader manager assigned.");
 	}
 	return instance;
 }
 
 void ResourceReader::Initialize(AAssetManager* manager)
 {
-	if (instance->manager == nullptr)
-		// Warning that manager has already been assigned.
-		;
+	if (instance->manager != nullptr)
+		PMdebug::MsgWarning("ResourceReader manager has already been initialized.");
 	instance->manager = manager;
 }
 
@@ -47,13 +51,11 @@ std::string ResourceReader::String(std::string fileName)
 		AAsset_close(tempAsset); // Destroy asset, no delete neccessary.
 
 		std::string tempString(tempBuffer.begin(), tempBuffer.end()); // Create string from buffer.
+		PMdebug::MsgInfo(tempString.c_str()); // Prints processed text.
 		return tempString;
 	}
 	else
-	{
-		// Error log.
-		return std::string();
-	}
+		return std::string(); // OpenAsset() handles warning messages.
 }
 
 LoadedImage ResourceReader::PNG(std::string fileName)
@@ -72,38 +74,43 @@ LoadedImage ResourceReader::PNG(std::string fileName)
 		AAsset_close(tempAsset);
 
 		unsigned int width, height; // Make note of image dimensions.
-		unsigned error = lodepng::decode(tempBuffer, width, height, decodedImage);
-
-
+		unsigned int error = lodepng::decode(tempBuffer, width, height, decodedImage); // Decode the raw data.
+		
+		if (error) // Warn about errors.
+			PMdebug::MsgWarning("PNG decoder error ", error, ": ", lodepng_error_text(error), " (", fileName.c_str(), ")");
+		
+		return LoadedImage(decodedImage, width, height);
 	}
 	else
-	{
-		// Error log.
-		//return;
-	}
+		return LoadedImage();
 }
 
 bool ResourceReader::ManagerCheck()
 {
-	return true;
-	// Check if manager has been assigned before usage.
-	// Add debug.
+	if (instance->manager != nullptr)
+		return true;
+	else
+	{
+		PMdebug::MsgWarning("Trying to use ResourceReader without initializing!");
+		return false;
+	}
 }
 
 AAsset* ResourceReader::OpenAsset(std::string fileName)
 {
 	if (ManagerCheck())
 	{
-		AAsset* tempAsset = AAssetManager_open(manager, fileName.c_str(), AASSET_MODE_UNKNOWN);
-		if (tempAsset != nullptr)
+		AAsset* tempAsset = AAssetManager_open(manager, fileName.c_str(), AASSET_MODE_UNKNOWN); // Open asset using manager.
+		size_t tempSize = AAsset_getLength(tempAsset); // Check asset length.
+
+		if (tempAsset != nullptr && tempSize > 0) // Asset opened succesfully and size greater than 0.
 		{
-			//LOGI("Luettu tiedosto %s (%d).", fileName.c_str(), size);
-			//LOGI("%s", buffer);
-			return tempAsset;
+			PMdebug::MsgInfo("Succesfully read file: ", fileName.c_str(), " (", tempSize, ")");
+			return tempAsset; // Return asset pointer for further use.
 		}
 		else
 		{
-			//LOGW("Tiedoston %s lukeminen ei onnistunut.", fileName.c_str());
+			PMdebug::MsgWarning("Reading file failed: ", fileName.c_str());
 			return nullptr;
 		}
 	}
