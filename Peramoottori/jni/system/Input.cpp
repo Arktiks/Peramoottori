4f398d3e1fab3739b0bc570d1455d744dbd27467
Althasandrian <Althasandrian@gmail.com>
2015-03-12 10:28:04 +0200

    gitpls

--- Peramoottori/jni/system/Input.cpp
+++ Peramoottori/jni/system/Input.cpp
@@ -1,12 +1,28 @@
 #include "Input.h"
+#include <android/input.h>
+
 using namespace pm;
+
+float Input::_x = 0;
+float Input::_y = 0;
+float Input::lx = 0;
+float Input::ly = 0;
+bool Input::touch = false;
+float Input::startOfDragX = 0;
+float Input::startOfDragY = 0;
 
 Input::Input()
 {
+	dragX = 0;
+	dragY = 0;
 }
-
 Input::~Input()
 {
+}
+
+glm::vec2 Input::GetTouchCoordinates()
+{
+	return glm::vec2(_x, _y);
 }
 
 bool Input::IsTouching()
@@ -14,28 +30,28 @@ bool Input::IsTouching()
 	return touch;
 }
 
-bool Input::SingleTouch()
-{
-	return singleTouch;
-}
 
 /// Static functions used in Application.cpp 
-void Input::InputEvent(float X, float Y)
+void Input::InputEventMovement(float x, float y)
+{
+	_x = x;
+	_y = y;
+}
+/// Static functions used in Application.cpp 
+void Input::InputEventKeyDown()
 {
 	touch = true;
-	x = X;
-	y = Y;
+	startOfDragX = _x;
+	startOfDragY = _y;
 }
-
+/// Static functions used in Application.cpp 
+void Input::InputEventKeyUp()
+{
+	touch = false;
+}
 void Input::Update()
 {
-	if (touch == true)
-		singleTouch = false;
-	else
-		singleTouch = true;
+	lx = _x;
+	ly = _y;
 
-	touch = false;
-
-	lx = x; 
-	ly = y;
 }
--- Peramoottori/jni/system/Input.h
+++ Peramoottori/jni/system/Input.h
@@ -1,39 +1,38 @@
 #ifndef INPUT_H
 #define INPUT_H
 
-#include <glm\vec2.hpp>
-
+#include "glm\vec2.hpp"
 namespace pm
 {
+	/// Need to decide wether we will make this class partialy or completely static
 	class Input
 	{
 	public:
 		Input();
 		~Input();
 
-		/// Change to suitable vec2?
-		void GetTouchCoordinates();
-
+		glm::vec2 GetTouchCoordinates();
 		bool IsTouching();
 
-		/// Works only once per loop reliably per input system.
-		bool SingleTouch();
 
 		/// Static member functions
 
 		/// Updates the static parts at start of main loop. /-> Application.cpp
 		static void Update();
-		/// Called when android touch event is received on android side. /-> Application.cpp
-		static void InputEvent(float X, float Y);
+		/// Called when android touch event (AKEY_EVENT_ACTION_UP) is received on android side. /-> Application.cpp
+		static void InputEventKeyUp();
+		/// Called when android touch event (AKEY_EVENT_ACTION_DOWN) is received on android side. /-> Application.cpp
+		static void InputEventKeyDown();
+		/// Called when android touch event (AINPUT_EVENT_TYPE_MOTION) is received on android side. /-> Application.cpp
+		static void InputEventMovement(float x, float y);
 
 	private:
 		/// Private variables(of multiple input systems if wanted)
-		
+		float dragX, dragY;
+
 		/// Static member variables
 		static bool touch;
-		static bool singleTouch;
-		static float x, y, lx, ly;
+		static float startOfDragX, startOfDragY, _x, _y, lx, ly;
 	};
 }
-
 #endif
