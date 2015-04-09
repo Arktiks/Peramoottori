c31b092420f9fec389707f442343a8d4f78511b2
ChiefCashier <jonipetteri.luomala@gmail.com>
2015-04-09 09:43:42 +0300

    texture ja texturefacktory

---
+++ Peramoottori/jni/resources/TextureFactory.cpp
@@ -0,0 +1,37 @@
+#include "TextureFactory.h"
+#include "resources\ResourceManager.h"
+
+pm::Texture TextureFactory::CreateTexture(std::string fileName)
+{
+	pm::Texture tempTexture;
+
+	pm::ImageResource* decodedImage = (pm::ImageResource*)pm::ResourceManager::GetInstance()->ReadAsset(fileName);
+
+	glm::vec2 asd;
+
+	std::vector<unsigned char> Image;
+	
+	unsigned error = lodepng::decode(Image, asd.x, asd.y, decodedImage->getImageData());
+
+
+	if (error) // display error to debugger;
+	{
+		DEBUG_WARNING(("Texture Creation failed"));
+	}
+	else
+	{
+		GLuint textureIndex;
+		glGenTextures(1, &textureIndex);
+		glBindTexture(GL_TEXTURE_2D, textureIndex);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
+			asd.x, asd.y,
+			0, GL_RGBA, GL_UNSIGNED_BYTE,
+			Image.data());
+
+		return tempTexture;
+	}
+
+	
+}
