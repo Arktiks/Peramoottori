a8991ebe125c13d013c50ce827b8b57e5adebc14
Althasandrian <Althasandrian@gmail.com>
2015-04-24 12:43:48 +0300

    ProfilerManager toimii, ainakin teoriassa...

---
+++ Peramoottori/jni/core/ProfilerManager.cpp
@@ -0,0 +1,40 @@
+#include "ProfilerManager.h"
+#include "core\Profiler.h"
+
+
+using namespace pm;
+using namespace std;
+
+std::map<std::string, long double> ProfilerManager::data = { { "initializing", 0.0f } };
+
+void ProfilerManager::GatherData(std::string name, long double value)
+{
+	data[name] = value;
+}
+
+void ProfilerManager::Print()
+{
+	map <string, long double>::iterator it;
+	for (it = data.begin(); it != data.end(); ++it)
+		DEBUG_INFO(("Time spent in %s is: %f", it->first.c_str(), it->second));
+}
+void ProfilerManager::Clear()
+{
+	data.clear();
+}
+void ProfilerManager::AdvPrint()
+{
+
+	map <string, long double>::iterator it;
+	long double total = 0;
+	for (it = data.begin(); it != data.end(); ++it)
+		total += it->second;
+
+
+	map <string, long double>::iterator it;
+	for (it = data.begin(); it != data.end(); ++it)
+	{
+		long double percentage = (it->second / total) * 100;
+		DEBUG_INFO(("Time spent in %s is: %f, which is %f percentage of total time.", it->first.c_str(), it->second, percentage));
+	}
+}
---
+++ Peramoottori/jni/core/ProfilerManager.h
@@ -0,0 +1,24 @@
+#ifndef PROFILERMANAGER_H
+#define PROFILERMANAGER_H
+
+#include <string>
+#include <map>
+
+namespace pm
+{
+	class ProfilerManager
+	{
+	public:
+		ProfilerManager(){};
+		~ProfilerManager(){};
+		static void GatherData(std::string name, long double value);
+		static void Print();
+		static void AdvPrint();
+		static void Clear();
+
+	private:
+		static std::map<std::string, long double> data;
+	};
+
+}
+#endif
