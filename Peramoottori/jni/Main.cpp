b9b0923d1ae98daad775c60ff9686efab595809f
Grimcode <tti13stuukkak@kamk.fi>
2015-05-08 10:42:50 +0300

    Gametest main.

--- Peramoottori/jni/Main.cpp
+++ Peramoottori/jni/Main.cpp
@@ -9,16 +9,7 @@
 using namespace pm;
 using namespace std;
 
-/////////////////////// Tuukka ///////////////////////
-#include <scene\GameEntity.h>
-#include <scene\Transformable.h>
-#include <resources\TextureFactory.h>
-#include <graphics\Rectangle.h>
-#include <graphics\Drawable.h>
-#include <graphics\SpriteBatch.h>
-#include <graphics\Text.h>
-
-void android_main(android_app* application)
+/*void android_main(android_app* application)
 {
 	DEBUG_INFO(("Starting android_main."));
 
@@ -42,6 +33,86 @@ void android_main(android_app* application)
 		for (int i = 0; i < teksti->GetTextVector().size(); i++)
 			SpriteBatch::GetInstance()->AddGameEntity(teksti->GetTextVector()[i]);
 
+		game->Draw();
+	}
+
+	DEBUG_INFO(("Exiting android_main."));
+}*/
+
+/////////////////////// Tuukka ///////////////////////
+#include <scene\GameEntity.h>
+#include <scene\Transformable.h>
+#include <resources\TextureFactory.h>
+#include <graphics\Rectangle.h>
+#include <graphics\Drawable.h>
+#include <graphics\SpriteBatch.h>
+#include <graphics\Color.h>
+
+namespace pm
+{
+	class GameClass // Test holding game information.
+	{
+	public:
+		GameClass() : rotation(0.0f)
+		{
+			objects.push_back(GameEntity());
+			objects[0].AddComponent(NEW Transformable(glm::vec2(500.0f, 500.0f), glm::vec2(1.0f, 1.0f), 0.0f));
+			objects[0].AddComponent(NEW Color(glm::vec4(0.5f, 0.5f, 0.5f, 1.0f)));
+			objects[0].AddComponent(TextureFactory::CreateTexture("iiro.png"));
+			objects[0].AddComponent(NEW Rectangle(150.0f, 150.0f));
+			objects[0].AddComponent(NEW Drawable);
+		};
+
+		void Update()
+		{
+			rotation += 0.1f;
+			objects[0].GetComponent<Transformable>()->SetRotation(rotation);
+			SpriteBatch::GetInstance()->AddGameEntity(&objects[0]);
+			//DEBUG_INFO(("Position: %f, %f", objects[0].GetComponent<Transformable>()->GetPosition().y,
+				//objects[0].GetComponent<Transformable>()->GetPosition().y));
+		};
+
+		void Draw()
+		{
+		};
+
+		std::vector<GameEntity> objects;
+		float rotation;
+
+		static bool first;
+	};
+
+	bool GameClass::first = false;
+}
+//////////////////////////////////////////////////////
+
+void android_main(android_app* application)
+{
+	DEBUG_INFO(("Starting android_main."));
+
+	Application* game = Application::GetInstance(); // For ease of use.
+	game->Initialize(application); // Contains loop which makes sure to initialize OpenGL and all modules.
+
+	game->Wait();
+
+	if (GameClass::first == false)
+	{
+		GameClass* g = NEW GameClass;
+		game->saveData = (void*)g;
+		GameClass::first = true;
+	}
+
+	Time time;
+	float fps = 0;
+
+	while (game->Update())
+	{
+		GameClass* access = (GameClass*)game->saveData;
+		access->Update();
+
+		//glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+
+		game->window.Clear();
 		game->Draw();
 	}
 
