5e8dedb39f4cc22523f4045a58c7a5d6a319cb9b
Ikutzu <ikutzu@hotmail.com>
2015-09-18 13:45:58 +0300

    Separated TextureCoordinates from Texture.
    Now TextureCoordinates and Texture are their own components. glTexture coordinates are calculated in SpriteBatch.

---
+++ Peramoottori/jni/scene/TextureCoordinates.cpp
@@ -0,0 +1,34 @@
+#include <scene\TextureCoordinates.h>
+
+pm::TextureCoordinates::TextureCoordinates(float left, float top, float right, float bottom)
+{
+	SetTextureCoordinates(left, top, right, bottom);
+}
+
+pm::TextureCoordinates::TextureCoordinates(glm::vec2 leftTop, glm::vec2 rightBottom)
+{
+	SetTextureCoordinates(leftTop[0], leftTop[1], rightBottom[0], rightBottom[1]);
+}
+
+pm::TextureCoordinates::TextureCoordinates(glm::vec4 coordinates)
+{
+	SetTextureCoordinates(coordinates[0], coordinates[1], coordinates[2], coordinates[3]);
+}
+
+void pm::TextureCoordinates::SetTextureCoordinates(float left, float top, float right, float bottom)
+{
+	textureCoordinates[0] = left;
+	textureCoordinates[1] = top;
+	textureCoordinates[2] = right;
+	textureCoordinates[3] = bottom;
+}
+
+void pm::TextureCoordinates::SetTextureCoordinates(glm::vec2 leftTop, glm::vec2 rightBottom)
+{
+	SetTextureCoordinates(leftTop[0], leftTop[1], rightBottom[0], rightBottom[1]);
+}
+
+void pm::TextureCoordinates::SetTextureCoordinates(glm::vec4 coordinates)
+{
+	SetTextureCoordinates(coordinates[0], coordinates[1], coordinates[2], coordinates[3]);
+}
