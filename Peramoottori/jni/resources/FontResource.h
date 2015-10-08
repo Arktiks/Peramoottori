d2c24fe6da33ea7400f12a686711b6a804a1d0fd
ChiefCashier <tti13sjoni-petteril@AOTA1-1LDML-29D.kamit.fi>
2015-10-08 11:19:03 +0300

    Commits plz

--- Peramoottori/jni/resources/FontResource.h
+++ Peramoottori/jni/resources/FontResource.h
@@ -3,7 +3,11 @@
 
 #include <resources\Resource.h>
 #include <vector>
+#include <map>
 
+#include <core\Log.h>
+#include <core\Memory.h>
+#include <GLES2\gl2.h>
 #include <include\ft2build.h>
 #include <scene\Texture.h>
 #include FT_FREETYPE_H
@@ -12,6 +16,12 @@ namespace pm
 {
 	class FontResource : public Resource
 	{
+		struct fontData
+		{
+			Texture* tex;
+			float cw, ch;
+		};
+
 	public:
 
 		/// constructor
@@ -19,7 +29,7 @@ namespace pm
 			ttfData(ttfData)
 		{
 			SetColor(1, 0, 1, 0);
-			//
+			CharTexture(ttfData);
 		};
 
 		///returns FT_Byte vector, that is the data of the font
@@ -33,6 +43,29 @@ namespace pm
 			color.at(2) = blue;
 			color.at(3) = alpha;
 		};
+
+		Texture* GetTexture(char c)
+		{
+			if (dataMap.find(c) != dataMap.end())
+				return dataMap[c].tex;
+			else
+				return NEW Texture;
+		}
+		float Getasd(char c)
+		{
+			if (dataMap.find(c) != dataMap.end())
+				return dataMap[c].cw;
+			else
+				return 1;
+		}
+
+		float Getasda(char c)
+		{
+			if (dataMap.find(c) != dataMap.end())
+				return dataMap[c].ch;
+			else
+				return 1;
+		}
 
 		/// return float vector, thats first four values are the rgba values of the font
 		std::vector<float> GetColor() { return color; };
@@ -54,11 +87,97 @@ namespace pm
 			color.push_back(blue);
 			color.push_back(alpha);
 		};
+		void CharTexture(std::vector<FT_Byte> ttfData)
+		{
+			FT_Library  library;
+		
+			FT_Error error = FT_Init_FreeType(&library);
+			if (error)
+				DEBUG_INFO(("Failed to initialize freetype library!"));
+		
+			std::vector<FT_Byte> ttf = ttfData;
+		
+			
+
+			FT_UInt glyph_index;
+	
+			std::string str("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUWXYZ1234567890!'#¤%&/()=?,.;:-^_{}|[]~@£$");
+
+			for (int i = 0; i < str.size(); i++)
+			{                                                      
+				error = FT_New_Memory_Face(library,
+					&ttf[0],
+					ttf.size(),
+					0,
+					&face);
+				if (error == FT_Err_Unknown_File_Format)
+				{
+					DEBUG_INFO(("The font file could be opened and read, but it appears that its font format is unsupported"));
+				}
+				else if (error)
+				{
+					DEBUG_INFO(("Font file could not be opened or read, or it is broken"));
+				}
+		
+				FT_GlyphSlot  slot = face->glyph;
+				
+				error = FT_Set_Char_Size(
+					face,						/* handle to face object           */
+					0,							/* char_width in 1/64th of points  */
+					32 * 64,					/* char_height in 1/64th of points */
+					1280,						/* horizontal device resolution    */
+					720);						/* vertical device resolution      */
+		
+		
+				glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+				glyph_index = FT_Get_Char_Index(face, str.at(i));
+
+				glGenTextures(1, &textId);
+				glActiveTexture(textId);
+				glBindTexture(GL_TEXTURE_2D, textId);
+		
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		
+				float ux = slot->bitmap_left;
+				float uy = slot->bitmap_top;
+		
+
+				
+
+				FT_Load_Glyph(face, glyph_index, FT_LOAD_RENDER);
+				
+				FT_Render_Glyph(slot, FT_RENDER_MODE_NORMAL);
+		
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, slot->bitmap.width, slot->bitmap.rows, 0, GL_ALPHA, GL_UNSIGNED_BYTE, slot->bitmap.buffer);
+
+				float uw = slot->bitmap.width;
+				float uh = slot->bitmap.rows;
+
+
+		
+				Texture *textTexture = NEW Texture();
+				textTexture->SetId(textId);
+				textTexture->SetTextureSize(glm::vec2(slot->bitmap.width, slot->bitmap.rows));
+				fontData fd;
+				fd.tex = textTexture;
+				fd.cw = uw;
+				fd.ch = uh;
+				dataMap[str.at(i)] = fd;
+			}
+		};
+
+
 	
 		std::vector<FT_Byte> ttfData;
 		std::vector<float> color;
-		std::vector<Texture*> texture;
+		std::map<char, fontData> dataMap;
 		std::string fileName;
+		FT_Face face;
+		GLuint textId;
 		
 	};
 }
